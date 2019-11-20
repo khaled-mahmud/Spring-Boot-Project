ed2087854f72d246f3910ada6ca66e737bba7e8d
khaled-mahmud <kmarif.cu@gmail.com>
2019-11-21 05:03:49 +0600

    Update_02

--- demo/src/main/java/com/testarif/demo/TopicController.java
+++ demo/src/main/java/com/testarif/demo/TopicController.java
@@ -3,6 +3,7 @@ package com.testarif.demo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,12 @@ public class TopicController {
   @RequestMapping("/topics")
   public List<Topic> getAllTopics(){
     return topicService.getAllTopics();
-	}
+  }
+  
+  @RequestMapping("/topics/{id}")
+  public Topic getTopic(@PathVariable String id){
+    return topicService.getTopic(id);
+  }
 
 }
 
--- demo/src/main/java/com/testarif/demo/TopicService.java
+++ demo/src/main/java/com/testarif/demo/TopicService.java
@@ -18,4 +18,8 @@ public class TopicService{
         return topics;
     }
 
+    public Topic getTopic(String id){
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
 }
